88e38f1f66516bff13782d239afd072678527684
omarch94 <omarcherti@gmail.com>
2024-02-10 14:45:11 +0100

    dark image hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,29 +1,36 @@
-import Button from "./Button"
+import Button from "./Button";
 
 interface Props {
-  scrollToAbout: () => void
+  scrollToAbout: () => void;
 }
 
 const Hero = ({ scrollToAbout = () => {} }: Props) => {
   return (
     <section className="bg-white dark:bg-neutral-900 py-20 lg:py-20">
       <div className="relative grid max-w-screen-xl px-4 mx-auto lg:gap-8 xl:gap-0 lg:grid-cols-12">
-        <div className="mx-auto place-self-center lg:col-span-7">
+        <div className="lg:col-span-7">
+        
           <h1 className="lg:max-w-2xl mb-4 text-4xl font-normal tracking-normal md:text-5xl xl:text-6xl">
             I am Omar Cherti a Full Stack Web developer From Morocco!
           </h1>
+          <div className="lg:hidden">
+            <div className="relative mb-8">
+              <img src="/images/omarcherti.png" alt="hero" className="transition-all duration-700 opacity-0 animating-image mx-auto border-transparent rounded-3xl" />
+              <div className="bg-neutral-950 absolute inset-0 transition-all duration-700 animating-bg w-full border-transparent rounded-3xl" />
+            </div>
+          </div>
           <p className="lg:max-w-2xl mb-6 font-normal leading-relaxed text-gray-500 lg:mb-8 text-xl">
-           A skilled full-stack web developer from Morocco, adept at crafting dynamic and engaging web applications. My expertise spans both front-end and back-end technologies, enabling me to build comprehensive solutions from concept to deployment.          </p>
+            A skilled full-stack web developer from Morocco, adept at crafting dynamic and engaging web applications. My expertise spans both front-end and back-end technologies, enabling me to build comprehensive solutions from concept to deployment.
+          </p>
         </div>
-  <div className="lg:order-first lg:mt-0 lg:col-span-5 relative flex justify-center  ">
-  <img src="/images/omarcherti.png" alt="hero" className="transition-all duration-700 opacity-0 animating-image mx-5 border-transparent rounded-3xl	 " />
-  <div className="bg-neutral-950 absolute inset-0 transition-all duration-700 animating-bg w-full border-transparent rounded-3xl 	" />
-</div>
-
-        <Button runFunc={scrollToAbout} classNames="lg:absolute w-24 h-24"/>
+        <div className="lg:order-first lg:mt-0 lg:col-span-5 hidden lg:block relative flex justify-center">
+          <img src="/images/omarcherti.png" alt="hero" className="transition-all duration-700 opacity-0 animating-image mx-5 border-transparent rounded-3xl" />
+          <div className="bg-neutral-950 absolute inset-0 transition-all duration-700 animating-bg w-full border-transparent rounded-3xl" />
+        </div>
+        <Button runFunc={scrollToAbout} classNames="lg:absolute w-24 h-24" />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;

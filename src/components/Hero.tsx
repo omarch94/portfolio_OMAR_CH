b02169bbecb95f475c5cd4c378f51a4b2abbf3be
omarch94 <omarcherti@gmail.com>
2024-02-10 14:21:54 +0100

    changes in about page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,14 +10,14 @@ const Hero = ({ scrollToAbout = () => {} }: Props) => {
       <div className="relative grid max-w-screen-xl px-4 mx-auto lg:gap-8 xl:gap-0 lg:grid-cols-12">
         <div className="mx-auto place-self-center lg:col-span-7">
           <h1 className="lg:max-w-2xl mb-4 text-4xl font-normal tracking-normal md:text-5xl xl:text-6xl">
-I am Omar Cherti a Full Stack Web developer From Morocco!
+            I am Omar Cherti a Full Stack Web developer From Morocco!
           </h1>
           <p className="lg:max-w-2xl mb-6 font-normal leading-relaxed text-gray-500 lg:mb-8 text-xl">
            A skilled full-stack web developer from Morocco, adept at crafting dynamic and engaging web applications. My expertise spans both front-end and back-end technologies, enabling me to build comprehensive solutions from concept to deployment.          </p>
         </div>
-  <div className="lg:order-first lg:mt-0 lg:col-span-5 relative ">
-  <img src="/images/omarcherti3.png" alt="hero" className="transition-all duration-700 opacity-0 animating-image mx-5 border-transparent " />
-  <div className="bg-neutral-950 absolute inset-0 transition-all duration-700 animating-bg w-full border-transparent rounded-3xl" />
+  <div className="lg:order-first lg:mt-0 lg:col-span-5 relative flex justify-center  ">
+  <img src="/images/omarcherti.png" alt="hero" className="transition-all duration-700 opacity-0 animating-image mx-5 border-transparent rounded-3xl	 " />
+  <div className="bg-neutral-950 absolute inset-0 transition-all duration-700 animating-bg w-full border-transparent rounded-3xl 	" />
 </div>
 
         <Button runFunc={scrollToAbout} classNames="lg:absolute w-24 h-24"/>
